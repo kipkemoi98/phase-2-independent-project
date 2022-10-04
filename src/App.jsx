bd22273b05b8811b51865c327f8570f953d6da11
kipkemoi98 <gilbert.kipkemoi@student.moringaschool.com>
2022-10-04 17:18:29 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ function App() {
     <div className='App'>
       <Navbar />
       <Routes>
-        <Route exact path='/' element={<LandingPage />}></Route>
-        <Route exact path='/add' element={<CreteRecipe />} ></Route>
+        <Route exact path='/' element={<HomePage />}></Route>
+        <Route exact path='/Add' element={<Recipe />} ></Route>
         <Route exact path='/recipes' element={<RecipeItems />} ></Route>
         <Route exact path='/contact' element={<ContactForm/>}></Route>
       </Routes>
